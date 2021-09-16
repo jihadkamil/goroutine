86ee3b385769b373152a3da4b9f4798184d1f862
jihad kamil <jihad.kamil@xapiens.id>
2021-09-16 17:30:58 +0700

    channel init

---
+++ channel_test.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"fmt"
+	"testing"
+	"time"
+)
+
+//  go test -v -run=TestCreateChannel
+func TestCreateChannel(T *testing.T) {
+	chn := make(chan string)
+	defer close(chn)
+
+	go func() { //anonymouse function
+		time.Sleep(2 * time.Second)
+		// channel must be taken so the code doesnt block
+		chn <- "hello channel"
+		fmt.Println("[done donk!]")
+	}() //auto run
+
+	// channel must be defined & sent so the code doesnt asleep - deadlock
+	data := <-chn
+	fmt.Println("data dari channel", data)
+
+	time.Sleep(4 * time.Second)
+}
+
+func SendParameter(channel chan string) {
+	time.Sleep(2 * time.Second)
+	// by default [chan] recieve data by reference not by value
+	channel <- "this is parameter"
+	fmt.Println("done donk!")
+}
+
+//  go test -v -run=TestChannelParameter
+func TestChannelParameter(t *testing.T) {
+	chn := make(chan string)
+	defer close(chn)
+	go SendParameter(chn)
+
+	data := <-chn
+	fmt.Println("data==>", data)
+
+	time.Sleep(4 * time.Second)
+
+}
