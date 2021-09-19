5d20418566c7efe8a8c0d30d77c35b58461d360a
jihad kamil <jihad.kamil@xapiens.id>
2021-09-19 17:27:36 +0700

    locking condition

---
+++ cond_test.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+	"testing"
+	"time"
+)
+
+var locker = sync.Mutex{}
+var group = sync.WaitGroup{}
+var cond = sync.NewCond(&locker)
+
+func WaitCondition(value int) {
+	defer group.Done()
+	group.Add(1)
+
+	cond.L.Lock()
+	cond.Wait()
+	fmt.Println("[[[[[Done]]]]]", value)
+	cond.L.Unlock()
+}
+
+func TestWaitCondition(t *testing.T) {
+	for i := 0; i < 10; i++ {
+		// group.Add(1)
+		go WaitCondition(i)
+	}
+
+	go func() {
+		for i := 0; i < 10; i++ {
+			time.Sleep(1 * time.Second)
+			// send signal to run 1 [one] goroutine, the rest will wait
+			cond.Signal()
+
+		}
+	}()
+
+	/*
+		will run all goroutine automatically
+		go func(){
+			time.Sleep(1 *time.Second)
+			cond.Broadcast()
+		}
+	*/
+
+	group.Wait()
+
+}
