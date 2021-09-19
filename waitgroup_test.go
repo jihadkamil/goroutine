4cc7c8fd3d768ce9601610cb34590c54677a8b5e
jihad kamil <jihad.kamil@xapiens.id>
2021-09-19 13:57:04 +0700

    wait group

---
+++ waitgroup_test.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+	"testing"
+)
+
+func RunAsynchronous(group *sync.WaitGroup, index int) {
+	defer group.Done()
+
+	group.Add(1)
+	fmt.Println("berhasil-------", index)
+	// 	time.Sleep(1 * time.Second)
+}
+
+//  go test -v -run=TestWaitGroup
+func TestWaitGroup(T *testing.T) {
+	group := &sync.WaitGroup{}
+
+	for i := 0; i < 10; i++ {
+		go RunAsynchronous(group, i)
+	}
+	// using group wait wil wait goroutine finish the process
+	group.Wait()
+	fmt.Println("[[[[[[[[[[[complete]]]]]]]")
+}
